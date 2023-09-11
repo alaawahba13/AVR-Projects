faea558a9583a81125fdc795cebcfa819a03d730
Alaa Wahba <alaawahbaa13@gmail.com>
2023-09-11 22:32:35 +0300

    MCAL

---
+++ Digital_Clock/Atmega32/inc/I2C.h
@@ -0,0 +1,33 @@
+/*
+ * I2C.h
+ *
+ *  Created on: 22 Jul 2023
+ *      Author: Alaa Wahba
+ */
+
+#ifndef ATMEGA32_INC_I2C_H_
+#define ATMEGA32_INC_I2C_H_
+
+#include "DIO.h"
+#include "Atmega32.h"
+#include "../../Library/BIT_MATH.h"
+
+#define MASTER_Mode
+ //#define SLAVE_Mode
+
+
+#define prescalar 		1		// Has values of 1 , 4 , 16 , 64
+#define SCL_Clock 			100000UL  //SCL frequency is 50K for 16Mhz
+
+void I2C_init();
+void I2C_Start();
+void I2C_Stop();
+void I2C_Write(uint8 data);
+uint8 I2C_Read(uint8 ack);
+void I2C_SendSlaveAddress(uint8 slaveAddress, uint8 Direction);
+//Slave Functions
+
+void I2C_setAddress(uint8 address);
+uint8 Slave_Read(uint8 ack);
+
+#endif /* ATMEGA32_INC_I2C_H_ */
